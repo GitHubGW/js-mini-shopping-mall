2805eff01c04114261f870d781b26cf4b2dfe3d0
GW <kowonp@gmail.com>
2021-11-26 21:22:39 +0900

    Fetch data, Display Items

---
+++ main.js
@@ -0,0 +1,21 @@
+const handleFetch = async () => {
+  const { items } = await (await fetch("/data/data.json")).json();
+  handleDisplayItems(items);
+};
+
+const handleDisplayItems = (items) => {
+  const itemsContainer = document.querySelector(".items");
+  const itemArray = items.map((item) => handleCreateHTMLString(item));
+  itemsContainer.innerHTML = itemArray.join();
+};
+
+const handleCreateHTMLString = (item) => {
+  return `
+    <li class="item">
+      <img src="${item.imageUrl}" alt="${item.type}" class="item__thumbnail" />
+      <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+  `;
+};
+
+handleFetch();
